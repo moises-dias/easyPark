97c74fdf02528b928b198d100d31b41e1660848e
unknown <thiagobubniak@alunos.utfpr.edu.br>
2021-11-01 13:47:34 -0300

    teste robo

--- robo.py
+++ robo.py
@@ -74,16 +74,16 @@ class Motor:
         self.rpi.set_PWM_dutycycle(self.input[4], 0)
 
     def turnLeft(self):
-        self.rpi.set_PWM_dutycycle(self.input[1], 255 * self.vel * 0.7)
+        self.rpi.set_PWM_dutycycle(self.input[1], 255 * self.vel * 0.74)
         self.rpi.set_PWM_dutycycle(self.input[2], 0)
-        self.rpi.set_PWM_dutycycle(self.input[3], 255 * self.vel * 0.7)
+        self.rpi.set_PWM_dutycycle(self.input[3], 255 * self.vel * 0.74)
         self.rpi.set_PWM_dutycycle(self.input[4], 0)
 
     def turnRight(self):
         self.rpi.set_PWM_dutycycle(self.input[1], 0)
-        self.rpi.set_PWM_dutycycle(self.input[2], 255 * self.vel * 0.7)
+        self.rpi.set_PWM_dutycycle(self.input[2], 255 * self.vel * 0.74)
         self.rpi.set_PWM_dutycycle(self.input[3], 0)
-        self.rpi.set_PWM_dutycycle(self.input[4], 255 * self.vel * 0.7)
+        self.rpi.set_PWM_dutycycle(self.input[4], 255 * self.vel * 0.74)
 
     def stop(self):
         self.rpi.set_PWM_dutycycle(self.input[1], 0)
@@ -306,8 +306,12 @@ class coneBot:
             self.motor.stop()
             sleep(10)
 
+    def test_tcrt(self):
+        print(self.tcrt.read())
+
 
 c = coneBot()
 c.test_motor()
+c.test_tcrt()
 
 #c.start()
