7c5fa2690014eb412bb62745489d0e3ae869c93a
unknown <thiagobubniak@alunos.utfpr.edu.br>
2021-11-01 15:53:32 -0300

    test color

--- robo.py
+++ robo.py
@@ -5,6 +5,11 @@ import math
 import json
 import pigpio
 
+from pathlib import Path
+import sys
+path_root = Path(__file__).parents[2]
+sys.path.append(str(path_root))
+
 from .TCS3200 import *
 
 # pigpio documentation
