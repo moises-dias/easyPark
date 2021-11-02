680fd99e661693baa5f37d6e5754efc803279dff
unknown <thiagobubniak@alunos.utfpr.edu.br>
2021-11-02 20:48:42 -0300

    teste linha

--- robo.py
+++ robo.py
@@ -423,9 +423,9 @@ class coneBot:
             if tcrt_read == [1,1,0,1,1]:
                 self.motor.go()
             elif tcrt_read == [1,0,1,1,1] or tcrt_read == [0,0,1,1,1] or tcrt_read == [0,1,1,1,1]:
-                self.motor.turnRight()
-            elif tcrt_read == [1,1,1,0,1] or tcrt_read == [1,1,1,0,0] or tcrt_read == [1,1,1,1,0]:
                 self.motor.turnLeft()
+            elif tcrt_read == [1,1,1,0,1] or tcrt_read == [1,1,1,0,0] or tcrt_read == [1,1,1,1,0]:
+                self.motor.turnRight()
             else:
                 self.motor.go()
 
