3f7925aa5f8cebc24bdaef34ca211a2179c45faa
unknown <thiagobubniak@alunos.utfpr.edu.br>
2021-11-02 20:46:59 -0300

    teste linha

--- robo.py
+++ robo.py
@@ -28,52 +28,6 @@ class Motor:
     def __init__(self, rpi, IN1, IN2, IN3, IN4):
         self.input = [-1, IN1, IN2, IN3, IN4]  # do not use self.input[0]
         self.rpi = rpi
-        self.vel = 1  # it ranges from 0 to 1
-        self.stop()  # garantir, vai q pino começa em high sei la
-
-        # preciso setar o modo dos pwm como output???
-        # boa pergunta, mas podemos testar, vai ser facil de achar
-
-    # podemos criar métodos para virar proporcionalmente dependendo do output do tcrt
-    # pelo que eu entendi:
-    # 1 = motor r frente
-    # 2 = motor r trás
-    # 3 = motor l frente
-    # 4 = motor l trás
-    def moveProportional(self, tcrt):
-        # tcrt é uma lista de bool com 5 itens, referentes a leitura do tcrt
-        # indo da esquerda para a direita
-        vel_l = 0
-        vel_r = 0
-        if tcrt[2]:  # se o IR central está vendo linha
-            # seta a velocidade no maximo
-            vel_r = 1
-            vel_l = 1
-        if tcrt[1]:  # se o primeiro sensor a esquerda do central está vendo linha
-            # diminui um pouco a velocidade do motor da esquerda, virar pra esquerda
-            vel_r = 1
-            vel_l = 0.8
-        if tcrt[3]:  # se o primeiro sensor a direita do central está vendo linha
-            # diminui um pouco a velocidade do motor da direta, virar para direita
-            vel_r = 0.8
-            vel_l = 1
-        if tcrt[0]:  # se o segundo sensor a esquerda do central esta vendo linha
-            # diminui mais ainda a velocidade do esquerdo
-            vel_r = 1
-            vel_l = 0.4
-        if tcrt[4]:  # se o segundo sensor a direita do central está vendo linha
-            # diminui mais ainda a velocidade do motor direito
-            vel_r = 0.4
-            vel_l = 1
-
-        # setando velocidades
-        self.rpi.set_PWM_dutycycle(self.input[1], 255 * vel_r)
-        self.rpi.set_PWM_dutycycle(self.input[2], 0)
-        self.rpi.set_PWM_dutycycle(self.input[3], 255 * vel_l)
-        self.rpi.set_PWM_dutycycle(self.input[4], 0)
-
-        # podemos dar um break/stop se não identificar nada, podemos testar isso
-
         self.vel_r = 1  # it ranges from 0 to 1
         self.vel_l = 1
         self.vel_max = 0.4
@@ -86,20 +40,16 @@ class Motor:
         self.rpi.set_PWM_dutycycle(self.input[4], 0)
 
     def turnLeft(self):
-        self.rpi.set_PWM_dutycycle(self.input[1], 255 * self.vel * 0.74 * self.vel_max)
+        self.rpi.set_PWM_dutycycle(self.input[1], 255 * self.vel_l * 0.74 * self.vel_max)
         self.rpi.set_PWM_dutycycle(self.input[2], 0)
-        self.rpi.set_PWM_dutycycle(self.input[3], 255 * self.vel * 0.74 * self.vel_max)
+        self.rpi.set_PWM_dutycycle(self.input[3], 255 * self.vel_l * 0.74 * self.vel_max)
         self.rpi.set_PWM_dutycycle(self.input[4], 0)
 
     def turnRight(self):
         self.rpi.set_PWM_dutycycle(self.input[1], 0)
-        self.rpi.set_PWM_dutycycle(
-            self.input[2], 255 * self.vel_l * 0.74 * self.vel_max
-        )
+        self.rpi.set_PWM_dutycycle(self.input[2], 255 * self.vel_l * 0.74 * self.vel_max)
         self.rpi.set_PWM_dutycycle(self.input[3], 0)
-        self.rpi.set_PWM_dutycycle(
-            self.input[4], 255 * self.vel_r * 0.74 * self.vel_max
-        )
+        self.rpi.set_PWM_dutycycle(self.input[4], 255 * self.vel_r * 0.74 * self.vel_max)
 
     def stop(self):
         self.rpi.set_PWM_dutycycle(self.input[1], 0)
@@ -124,10 +74,21 @@ class Motor:
             vel = 1
         elif vel < 0:
             vel = 0
-        # acho q levantar um raise pode travar o robo no meio do estacionamento
-        # if vel > 1 or vel < 0:
-        #     raise "Seting motor velocity outside 0.0 - 1.0 range!"
-        self.vel = vel
+        self.vel_l = vel
+
+    def setVelRight(self, vel):
+        if vel > 1:
+            vel = 1
+        elif vel < 0:
+            vel = 0
+        self.vel_r = vel
+
+    def setVelMax(self, vel):
+        if vel > 1:
+            vel = 1
+        elif vel < 0:
+            vel = 0
+        self.vel_max = vel
 
 
 class ColorSensor:
@@ -175,14 +136,9 @@ class ColorSensor:
         self.rpi.write(self.s[2], 0)
         self.rpi.write(self.s[3], 1)
         return self.rpi.get_PWM_dutycycle(self.out)
-        self.vel_l = vel
 
-    def setVelRight(self, vel):
-        if vel > 1:
-            vel = 1
-        elif vel < 0:
-            vel = 0
-        self.vel_r = vel
+    def readAll(self):
+        return self.readRed(), self.readGreen(), self.readBlue()
 
     def color(self):
         colors = list(self.readAll())  # returns red, green, blue into a list
@@ -217,13 +173,6 @@ class ColorSensor:
                 return "white"
             else:
                 return "black"
-
-    def setMaxVel(self, vel):
-        if vel > 1:
-            vel = 1
-        elif vel < 0:
-            vel = 0
-        self.vel_max = vel
 
 
 class Tcrt5000:
@@ -303,38 +252,20 @@ class coneBot:
 
         self.rpi = pigpio.pi()
 
-        self.motor = Motor(
-            self.rpi, 2, 3, 4, 17
-        )  # RPi pins for [IN1, IN2, IN3, IN4] motor driver
-        self.tcrt = Tcrt5000(
-            self.rpi, 5, 6, 13, 19, 26
-        )  # RPi pins for [S1, S2, S3, S4, S5] tcrt5000 module
-        self.color = ColorSensor(
-            self.rpi, 27, 22, 10, 9, 11
-        )  # RPi pins for [S0, S1, S2, S3] and OUT
+        self.motor = Motor(self.rpi, 2, 3, 4, 17)  # RPi pins for [IN1, IN2, IN3, IN4] motor driver
+        self.tcrt = Tcrt5000(self.rpi, 5, 6, 13, 19, 26)  # RPi pins for [S1, S2, S3, S4, S5] tcrt5000 module
+        self.color = ColorSensor(self.rpi, 27, 22, 10, 9, 11)  # RPi pins for [S0, S1, S2, S3] and OUT
         self.ultra = Ultrasonic(self.rpi, 23, 24)  # RPi pins for trig and echo
+
+        # acelerometer fazer dpois, focar primeiro no motor e IR
+        # self.acelerometer = Acelerometer(self.rpi, self.acel)
 
         self.location_system = RobotLocationSystem(graph, directions)
         if TESTING_NETWORK:
             self.notification_listener = RobotNotificationListener()
             self.notification_listener.listen()
-        self.motor = Motor(
-            self.rpi, 2, 3, 4, 17
-        )  # RPi pins for [IN1, IN2, IN3, IN4] motor driver
-        self.tcrt = Tcrt5000(
-            self.rpi, 5, 6, 13, 19, 26
-        )  # RPi pins for [S1, S2, S3, S4, S5] tcrt5000 module
-        self.color = ColorSensor(
-            self.rpi, 11, 10, 9, 27, 22
-        )  # RPi pins for OUT, S2, S3, S0, S1
-        self.ultra = Ultrasonic(self.rpi, 23, 24)  # RPi pins for trig and echo
 
-        # acelerometer fazer dpois, focar primeiro no motor e IR
-        # self.acelerometer = Acelerometer(self.rpi, self.acel)
-
-        # acelerometer fazer dpois, focar primeiro no motor e IR
-        # self.acelerometer = Acelerometer(self.rpi, self.acel)
-
+        self.motor.stop()
         # self.start()
 
     def start(self):
@@ -367,8 +298,6 @@ class coneBot:
             print(self.tcrt.read())
 
     def test_color(self):
-        while 1:
-            print(self.color.readAll(), self.color.color())
 
         input("Calibrating black object, press RETURN to start")
         hz = self.color.get_hertz()
