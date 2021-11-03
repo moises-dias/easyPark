3fb9593e29db88282f220aebd0e77a02c5728007
unknown <thiagobubniak@alunos.utfpr.edu.br>
2021-11-02 21:08:45 -0300

    teste linha

--- robo.py
+++ robo.py
@@ -425,24 +425,45 @@ class coneBot:
                 self.motor.setVelRight(1)
                 self.motor.go()
 
-            elif tcrt_read == [1,0,0,1,1] or tcrt_read == [1,0,1,1,1]:
+
+
+            elif tcrt_read == [1,0,0,1,1]:
                 self.motor.setVelLeft(1)
-                self.motor.setVelRight(0.7)
+                self.motor.setVelRight(0.8)
+                self.motor.go()
+            
+            elif tcrt_read == [1,0,1,1,1]:
+                self.motor.setVelLeft(1)
+                self.motor.setVelRight(0.6)
                 self.motor.go()
 
-            elif tcrt_read == [0,0,1,1,1] or tcrt_read == [0,1,1,1,1]:
+            elif tcrt_read == [0,0,1,1,1]:
+                self.motor.setVelLeft(1)
+                self.motor.setVelRight(0.4)
+                self.motor.go()
+
+            elif tcrt_read == [0,1,1,1,1]:
                 self.motor.turnLeft()
 
-            elif tcrt_read == [1,1,0,0,1] or tcrt_read == [1,1,1,0,1]:
-                self.motor.setVelLeft(0.7)
+
+
+            elif tcrt_read == [1,1,0,0,1]:
+                self.motor.setVelLeft(0.8)
                 self.motor.setVelRight(1)
                 self.motor.go()
 
-            elif tcrt_read == [1,1,1,0,1] or tcrt_read == [1,1,1,0,0] or tcrt_read == [1,1,1,1,0]:
-                self.motor.turnRight()
-
-            else:
+            elif tcrt_read == [1,1,1,0,1]:
+                self.motor.setVelLeft(0.6)
+                self.motor.setVelRight(1)
                 self.motor.go()
+
+            elif tcrt_read == [1,1,1,0,0]:
+                self.motor.setVelLeft(0.4)
+                self.motor.setVelRight(1)
+                self.motor.go()
+
+            elif tcrt_read == [1,1,1,1,0]:
+                self.motor.turnRight()
 
 c = coneBot()
 
