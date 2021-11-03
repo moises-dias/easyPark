0b688572aa68b1ce794274a14106966f78d2d723
unknown <thiagobubniak@alunos.utfpr.edu.br>
2021-11-02 21:24:56 -0300

    teste linha

--- robo.py
+++ robo.py
@@ -427,24 +427,24 @@ class coneBot:
                 self.motor.go()
 
             elif tcrt_read == [1,0,0,1,1] or tcrt_read == [1,0,1,1,1]:
-                self.motor.setVelLeft(1)
-                self.motor.setVelRight(0.5)
-                self.motor.go()
-
-            elif tcrt_read == [0,0,1,1,1] or tcrt_read == [0,1,1,1,1]:
-                self.motor.setVelLeft(1)
-                self.motor.setVelRight(0)
-                self.motor.go()
-
-
-            elif tcrt_read == [1,1,0,0,1] or tcrt_read == [1,1,1,0,1]:
                 self.motor.setVelLeft(0.5)
                 self.motor.setVelRight(1)
                 self.motor.go()
 
-            elif tcrt_read == [1,1,1,0,0] or tcrt_read == [1,1,1,1,0]:
+            elif tcrt_read == [0,0,1,1,1] or tcrt_read == [0,1,1,1,1]:
                 self.motor.setVelLeft(0)
                 self.motor.setVelRight(1)
+                self.motor.go()
+
+
+            elif tcrt_read == [1,1,0,0,1] or tcrt_read == [1,1,1,0,1]:
+                self.motor.setVelLeft(1)
+                self.motor.setVelRight(0.5)
+                self.motor.go()
+
+            elif tcrt_read == [1,1,1,0,0] or tcrt_read == [1,1,1,1,0]:
+                self.motor.setVelLeft(1)
+                self.motor.setVelRight(0)
                 self.motor.go()
 
 
