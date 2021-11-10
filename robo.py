5b379420ea3acdec2440fa16e5d1472f6758afa3
unknown <thiagobubniak@alunos.utfpr.edu.br>
2021-11-10 16:27:58 -0300

    testing color

--- robo.py
+++ robo.py
@@ -211,13 +211,13 @@ class coneBot:
 
         input("Calibrating black object, press RETURN to start")
         hz = self.color.get_hertz()
-        hz = [268.256, 247.957, 302.885]
+        #hz = [268.256, 247.957, 302.885]
         print(hz)
         self.color.set_black_level(hz)
 
         input("Calibrating white object, press RETURN to start")
         hz = self.color.get_hertz()
-        hz = [652.742, 633.967, 789.036]
+        #hz = [652.742, 633.967, 789.036]
         print(hz)
         self.color.set_white_level(hz)
 
