b48abb5f4c2cc33fe09ae39b056bd6ce53962b49
unknown <thiagobubniak@alunos.utfpr.edu.br>
2021-11-15 15:07:07 -0300

    testing line following

--- robo.py
+++ robo.py
@@ -541,10 +541,10 @@ c = coneBot()
 # c.test_motor()    # ok
 # c.test_tcrt()     # ok
 # c.test_color()    # ok
-c.test_buzzer()  # ok
+# c.test_buzzer()  # ok
 # c.test_ultrassom() # ok
 # c.test_gyro()     # ok
 # c.followLineDumb()
 # c.start()
 
-# c.moveOnParkingLot() # para testar o movimento no estacionamento
+c.moveOnParkingLot() # para testar o movimento no estacionamento
