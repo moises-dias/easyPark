7b605605cff343457b89219fcbb84c1f17d20c47
unknown <thiagobubniak@alunos.utfpr.edu.br>
2021-11-22 19:21:05 -0300

    testing parking lot with IR sensor

--- robo.py
+++ robo.py
@@ -388,14 +388,14 @@ class coneBot(Thread):
 
         elif tcrt_read == [1, 0, 0, 1, 1] or tcrt_read == [1, 0, 1, 1, 1]:
             self.motor.setVelLeft(1)
-            self.motor.setVelRight(0.5)
+            self.motor.setVelRight(0.8)
             self.motor.go()
 
         elif tcrt_read == [0, 0, 1, 1, 1] or tcrt_read == [0, 1, 1, 1, 1]:
             self.motor.turnLeft()
 
         elif tcrt_read == [1, 1, 0, 0, 1] or tcrt_read == [1, 1, 1, 0, 1]:
-            self.motor.setVelLeft(0.5)
+            self.motor.setVelLeft(0.8)
             self.motor.setVelRight(1)
             self.motor.go()
 
