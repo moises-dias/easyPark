3c04c5c6b8bb79a4f23c504454bd82a169d50a97
unknown <thiagobubniak@alunos.utfpr.edu.br>
2021-11-22 19:19:30 -0300

    testing parking lot with IR sensor

--- robo.py
+++ robo.py
@@ -381,25 +381,23 @@ class coneBot(Thread):
     def followLineDumbSemWhileTrue(self):
         tcrt_read = self.tcrt.read()
 
-        if tcrt_read == [1, 1, 0, 1, 1] or tcrt_read == [1, 0, 0, 1, 1] or tcrt_read == [1, 1, 0, 0, 1]:
+        if tcrt_read == [1, 1, 0, 1, 1] :
             self.motor.setVelLeft(1)
             self.motor.setVelRight(1)
             self.motor.go()
 
-        elif tcrt_read == [1, 0, 1, 1, 1]:
-            #self.motor.setVelLeft(1)
-            #self.motor.setVelRight(0.8)
-            #self.motor.go()
-            self.motor.turnLeft()
+        elif tcrt_read == [1, 0, 0, 1, 1] or tcrt_read == [1, 0, 1, 1, 1]:
+            self.motor.setVelLeft(1)
+            self.motor.setVelRight(0.5)
+            self.motor.go()
 
         elif tcrt_read == [0, 0, 1, 1, 1] or tcrt_read == [0, 1, 1, 1, 1]:
             self.motor.turnLeft()
 
-        elif tcrt_read == [1, 1, 1, 0, 1]:
-            #self.motor.setVelLeft(0.8)
-            #self.motor.setVelRight(1)
-            #self.motor.go()
-            self.motor.turnRight()
+        elif tcrt_read == [1, 1, 0, 0, 1] or tcrt_read == [1, 1, 1, 0, 1]:
+            self.motor.setVelLeft(0.5)
+            self.motor.setVelRight(1)
+            self.motor.go()
 
         elif tcrt_read == [1, 1, 1, 0, 0] or tcrt_read == [1, 1, 1, 1, 0]:
             self.motor.turnRight()
