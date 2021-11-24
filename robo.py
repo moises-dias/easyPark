1d58e5eb13a8ac7273a31e39def881cf28a55ded
unknown <thiagobubniak@alunos.utfpr.edu.br>
2021-11-24 16:50:22 -0300

    ultrasound stop test

--- robo.py
+++ robo.py
@@ -376,7 +376,7 @@ class coneBot(Thread):
         ti = time.time()
         tf = time.time()
 
-        while (tf - ti) < 0.7:
+        while (tf - ti) < 1:
             tf = time.time()
 
         while not self.tcrt_side.read():  # Enquanto black
