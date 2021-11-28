4de358324aca89deb3bcc12f1dbe3fe09d98073d
moises.dias <moises.dias@dextra-sw.com>
2021-11-28 18:38:16 -0300

    test

--- robo.py
+++ robo.py
@@ -482,6 +482,15 @@ class coneBot(Thread):
         while self.tcrt_side.read():  # Enquanto white
             pass
 
+        if direction == "L":
+            self.motor.turnRight()
+            sleep(0.09)
+            # self.motor.turnLeft()
+        else:
+            self.motor.turnLeft()
+            sleep(0.09)
+            # self.motor.turnRight()
+
         self.motor.brake()
 
     def moveStraight(self):
