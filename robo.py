ba17d0423214faee5cc6549f6461690a8e6d16c1
unknown <thiagobubniak@alunos.utfpr.edu.br>
2021-11-30 18:31:05 -0300

    testing ultrasound + gyro + buzzer

--- robo.py
+++ robo.py
@@ -419,6 +419,7 @@ class coneBot(Thread):
         g = np.round(list(self.gyro.read_acc()), 4)
         print(abs(g[0]), abs(g[1]), self.ultra.measure_distance())
         while self.ultra.measure_distance() < 10.0 or abs(g[0]) > 0.35 or abs(g[1] > 0.35):  # Se entrar algo na frente, espera (pooling)
+            print(abs(g[0]), abs(g[1]), self.ultra.measure_distance())
             self.soundAlarm();
             self.motor.brake()
             sleep(0.20)
